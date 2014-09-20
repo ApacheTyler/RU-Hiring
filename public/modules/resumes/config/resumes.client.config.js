5eeed7ab458e27eb570d9770247e0992a9810f55
Tyler Freeman <tyler.freeman@modea.com>
2014-09-20 14:40:30 -0400

    Added resme's to topbar menu

--- public/modules/resumes/config/resumes.client.config.js
+++ public/modules/resumes/config/resumes.client.config.js
@@ -4,8 +4,8 @@
 angular.module('resumes').run(['Menus',
 	function(Menus) {
 		// Set top bar menu items
-		Menus.addMenuItem('My Resume'', 'Resumes', 'resumes', 'dropdown', '/resumes(/create)?');
-		Menus.addSubMenuItem('My Resume'', 'resumes', 'List Resumes', 'resumes');
-		Menus.addSubMenuItem('My Resume'', 'resumes', 'New Resume', 'resumes/create');
+		Menus.addMenuItem('topbar', 'Resumes', 'resumes', 'dropdown', '/resumes(/create)?');
+		Menus.addSubMenuItem('topbar', 'resumes', 'List Resumes', 'resumes');
+		Menus.addSubMenuItem('topbar', 'resumes', 'New Resume', 'resumes/create');
 	}
 ]);
